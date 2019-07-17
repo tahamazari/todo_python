43688229efbe580d45c1ee4045513b235daa4cf0
tahamazari <tahamazari@gmail.com>
2019-07-17 12:28:12 +0500

    minor edit

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ def main():
         print("Press 2 to add new task to your TODO List")
         print("Press 3 to update previous task")
         print("Press 4 to delete a task ")
-        print("Press 5 to exit")
+        print("Press any other number to exit")
 
         action = int(input())
 
@@ -25,7 +25,7 @@ def main():
         elif(action == 4):
             delete_task(tasks_dict)
 
-        elif(action == 5):
+        elif(action != 1 | 2 | 3 | 4):
             exit()
 
 
