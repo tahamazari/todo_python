86095897bf9cc4d545a85c9e8d4561c8883a4549
tahamazari <tahamazari@gmail.com>
2019-07-17 12:21:45 +0500

    all functionalities implemented

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def add_task(dict):
     print("Please type down your task")
     value = input()
     dict.update({key: value})
-    print(dict)
+    print("New task with title" + key + "added")
     return dict
 
 def update_task(dict):
@@ -21,50 +21,51 @@ def update_task(dict):
         print("Type in to update current task")
         value = input()
         dict.update({key: value})
-        print(dict)
+        print("Task with title " + key + " updated")
         return dict
     else: 
-        print("titele not found")
+        print("Title not found")
+    
 
 def delete_task(dict):
     print("Type in title of task you want to delete")
     key = input()
-    if key in dict:
+    if(len(dict) == 0):
+        print("Your TODO List is empty! Can't delete anything")
+    elif key in dict:
         del dict[key]
-        print(dict)
+        print("Task with title " + key + " deleted")
         return dict
     else: 
         print("Task with title " + key + " not found")
 
 tasks_dict = {}
-tasks_dict.update({'taha': 'mazari'})
-tasks_dict.update({'job': 'engineer'})
 
 def main():
-    print("Welcome to TODO List App!")
-    print("Choose from the following options from below:")
-    print("Press 1 to view your TODO List")
-    print("Press 2 to add new task to your TODO List")
-    print("Press 3 to update previous task")
-    print("Press 4 to delete a task ")
-    print("Press any other key to exit")
+    while True:
+        print("Choose from the following actions from below:")
+        print("Press 1 to view your TODO List")
+        print("Press 2 to add new task to your TODO List")
+        print("Press 3 to update previous task")
+        print("Press 4 to delete a task ")
+        print("Press 5 to exit")
 
-    action = int(input())
+        action = int(input())
 
-    if(action == 1):
-        list_tasks(tasks_dict)
+        if(action == 1):
+            list_tasks(tasks_dict)
 
-    elif(action == 2):
-        add_task(tasks_dict)
+        elif(action == 2):
+            add_task(tasks_dict)
 
-    elif(action == 3):
-        update_task(tasks_dict)
+        elif(action == 3):
+            update_task(tasks_dict)
 
-    elif(action == 4):
-        delete_task(tasks_dict)
+        elif(action == 4):
+            delete_task(tasks_dict)
 
-    else:
-        exit()
+        elif(action == 5):
+            exit()
 
 
 main()
